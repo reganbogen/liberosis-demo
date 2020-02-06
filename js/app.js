e3e32fbb648012e51ef3c88cb03990bab4b9e10b
Regan Layman <regan.layman@gmail.com>
2020-02-06 14:32:20 -0500

    corrected stylesheet link by added public/ folder at the beginning, updated console message on app.js, added darkgoldenrod background color to surround canvas, added organization in css file

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
+particlesJS.load('particles-js', 'js/particles.json', function() {
+  console.log('particles.js loaded - GREAT JOB!');
+});
+
