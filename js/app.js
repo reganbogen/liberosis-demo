b08fd0bf0bd96ecc318a7bc7883195b23b3db7b7
Regan Layman <regan.layman@gmail.com>
2020-07-22 21:47:03 -0400

    redoing liberosis in a final form with bootstrap framework for responsive design

--- js/app.js
+++
@@ -1,6 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-
-particlesJS.load('particles-js', 'js/particles.json', function() {
-  console.log('particles.js loaded - GREAT JOB!');
-});
-
