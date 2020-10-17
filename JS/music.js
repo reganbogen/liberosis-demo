e0f3b0e7ab9b8f1e3ba2c81610a61865459e142c
Regan Layman <regan.layman@gmail.com>
2020-10-17 17:09:23 -0400

    array with data from pixels exists now. now it has to loop through and add in all that aren't transparent.

--- JS/music.js
+++ JS/music.js
@@ -5,8 +5,23 @@ canvas.width = png.width;
 canvas.height = png.height;
 
 ctx.drawImage(png, 0, 0);
+
 var data = ctx.getImageData(0, 0, png.width, png.height);
+
 ctx.clearRect(0,0,canvas.width, canvas.height);
+
+var particles = [];
+for (var y = 0, y2 = data.height; y < y2; y++) {
+    for (var x = 0, x2 = data.width; x < x2; x++) {
+        if (data.data[(x * 4 + y * 4 * data.width) + 3] > 128) {
+            var particle = {
+                x : x,
+                y : y
+            };
+            particles.push(particle);
+        }
+    }
+}
 
 var png = new Image();
 png.onload = drawScene;
