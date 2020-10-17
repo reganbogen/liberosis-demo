825046e745e6fb4bd77487ea56467b714c8a4742
Regan Layman <regan.layman@gmail.com>
2020-10-17 17:06:38 -0400

    init the canvas scene

--- JS/music.js
+++ JS/music.js
@@ -1,3 +1,9 @@
+var canvas = document.getElementById("scene");
+var ctx = canvas.getContext("2d");
+
+canvas.width = png.width;
+canvas.height = png.height;
+
 var png = new Image();
 png.onload = drawScene;
 png.src = "img/gpigg.png";
