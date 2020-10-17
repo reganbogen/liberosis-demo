4efeb73901d4105746ccaddbaeaa330a327166c7
Regan Layman <regan.layman@gmail.com>
2020-10-17 17:08:16 -0400

    drew the imaage using getImageData from canvas API. returns object with an array in it with 4 values for each pixel, one for each color RGB and then one for alpha

--- JS/music.js
+++ JS/music.js
@@ -4,6 +4,10 @@ var ctx = canvas.getContext("2d");
 canvas.width = png.width;
 canvas.height = png.height;
 
+ctx.drawImage(png, 0, 0);
+var data = ctx.getImageData(0, 0, png.width, png.height);
+ctx.clearRect(0,0,canvas.width, canvas.height);
+
 var png = new Image();
 png.onload = drawScene;
 png.src = "img/gpigg.png";
