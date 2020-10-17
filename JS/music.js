3142f3ae2d8dd5acd618da343db6b72f540514a5
Regan Layman <regan.layman@gmail.com>
2020-10-17 17:17:36 -0400

    added in drawScene function, and checked it. great success.

--- JS/music.js
+++ JS/music.js
@@ -1,26 +1,35 @@
-var canvas = document.getElementById("scene");
-var ctx = canvas.getContext("2d");
+function drawScene() {
+    var canvas = document.getElementById("scene");
+    var ctx = canvas.getContext("2d");
 
-canvas.width = png.width;
-canvas.height = png.height;
+    canvas.width = png.width;
+    canvas.height = png.height;
 
-ctx.drawImage(png, 0, 0);
+    ctx.drawImage(png, 0, 0);
 
-var data = ctx.getImageData(0, 0, png.width, png.height);
+    var data = ctx.getImageData(0, 0, png.width, png.height);
 
-ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.clearRect(0,0,canvas.width, canvas.height);
 
-var particles = [];
-for (var y = 0, y2 = data.height; y < y2; y++) {
-    for (var x = 0, x2 = data.width; x < x2; x++) {
-        if (data.data[(x * 4 + y * 4 * data.width) + 3] > 128) {
-            var particle = {
-                x : x,
-                y : y
-            };
-            particles.push(particle);
+    var particles = [];
+    for (var y = 0, y2 = data.height; y < y2; y++) {
+        for (var x = 0, x2 = data.width; x < x2; x++) {
+            if (data.data[(x * 4 + y * 4 * data.width) + 3] > 128) {
+                var particle = {
+                    x : x,
+                    y : y
+                };
+                particles.push(particle);
+            }
         }
     }
+  
+  ctx.fillStyle = "white";
+  for(var i = 0, j= particles.length; i<j ; i++){
+    var particle = particles[i];
+    ctx.fillRect(particle.x, particle.y, 1, 1);
+  }
+
 }
 
 var png = new Image();
